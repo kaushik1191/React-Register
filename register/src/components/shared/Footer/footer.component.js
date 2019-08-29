02e84e6f09dfc093e0ed7e6a36ab511b87074f41
kaushik1191 <gopalsavaliya1198@gmail.com>
2019-08-29 15:11:52 +0530

    register form

---
+++ register/src/components/shared/Footer/footer.component.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+function Footer() {
+    return (
+        <Fragment>
+            <hr />
+            <small>&copy; Kaushik Savaliya</small>
+        </Fragment>
+    );
+}
+
+export default Footer; 
